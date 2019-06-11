e17e17e0cc2785bd8beeb9d68f6236c5c133e7af
seongjin571 <gkstjdwls11@naver.com>
2019-06-12 04:35:59 +0900

    D2

---
+++ hsj/src/D2/Solution_1288_새로운_불명증_치료법.java
@@ -0,0 +1,34 @@
+package D2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution_1288_새로운_불명증_치료법 {
+	static Set<Integer> setNum = new HashSet<>();
+	
+	static int getMinTime(int multiNum) {
+		int count = 0;
+		int result = multiNum;
+		while(setNum.size() != 10) {
+			count++;
+			result =  multiNum * count;
+			String stringResult = Integer.toString(result);
+			for(int i = 0; i < stringResult.length(); i++) {
+				setNum.add(stringResult.charAt(i)-'0');
+			}
+		}
+		setNum.clear();
+		return result;
+	}
+	
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		int testNum = scan.nextInt();
+		for(int testCount = 1; testCount <= testNum; testCount++) {
+			int multiNum = scan.nextInt();
+			System.out.println("#"+testCount+" "+getMinTime(multiNum));	
+		}
+	}
+
+}
---
+++ hsj/src/D2/Solution_1989_초심자의_회문_검사.java
@@ -0,0 +1,33 @@
+package D2;
+
+import java.util.Scanner;
+
+public class Solution_1989_초심자의_회문_검사 {
+	static int checkString(String testStr) {
+		int strLength = testStr.length();
+		int countS = strLength/2 - 1;
+		int countB;
+		
+		if(strLength%2 == 0) 
+			countB = strLength/2;
+		else 
+			countB = strLength/2 + 1;
+		
+		while(countS >= 0) {
+			if(testStr.charAt(countS) != testStr.charAt(countB)) return 0;
+			countS--;
+			countB++;
+		}
+		return 1;
+	}
+	
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+		int testNum = scan.nextInt();
+		for(int i = 1; i <= testNum; i++) {
+			String testStr = scan.next();
+			System.out.println("#" + i + " " + checkString(testStr));
+		}
+	}
+
+}
