4bf3fe6b80936df498a84c8878acca0c0ad0fdb4
seongjin571 <gkstjdwls11@naver.com>
2019-06-24 16:40:55 +0900

    week04

--- hsj/src/week04/Solution_1211_Ladder2.java
+++ hsj/src/week04/Solution_1211_Ladder2.java
@@ -2,14 +2,17 @@ package week04;
 
 import java.util.Scanner;
 
-public class Solution_1210_Ladder1 {
+public class Solution_1211_Ladder2 {
 	static Scanner scan = new Scanner(System.in);
 	static int[][] map = new int[100][100];
 	static int[] dx = { 0, 0, 1 };
 	static int[] dy = { 1, -1, 0 };
 	static boolean[][] visit;
-
-	static int doLadder(int startY) {
+	static int maxCountY;
+	static int beforeCountY;
+	
+	static void doLadder(int startY) {
+		int count = 1;
 		int x = 0;
 		int y = startY;
 		while (x < 99) {
@@ -17,23 +20,25 @@ public class Solution_1210_Ladder1 {
 				int newX = x + dx[i];
 				int newY = y + dy[i];
 				if ((-1 < newX && newX < 100) && (-1 < newY && newY < 100)
-						&& (map[newX][newY] != 0 && !visit[newX][newY])) {
-					if (map[newX][newY] == 2) {
-						return startY;
-					} else {
+						&& (map[newX][newY] == 1 && !visit[newX][newY])) {
+						count++;
 						visit[newX][newY] = true;
 						x = newX;
 						y = newY;
-					}
 				}
 			}
 		}
-		return 0;
+		if(count <= beforeCountY) {
+			beforeCountY = count;
+			maxCountY = startY;
+		}
 	}
 
 	public static void main(String[] args) {
 		int result = 0;
 		for (int i = 1; i <= 10; i++) {
+			maxCountY = 0;
+			beforeCountY = 10000;
 			int testCount = scan.nextInt();
 			for (int j = 0; j < 100; j++) {
 				for (int k = 0; k < 100; k++) {
@@ -44,12 +49,10 @@ public class Solution_1210_Ladder1 {
 				visit = new boolean[100][100];
 				if (map[0][z] == 1) {
 					visit[0][z] = true;
-					result = doLadder(z);
-					if (result != 0)
-						break;
+					doLadder(z);
 				}
 			}
-			System.out.println("#" + i + " " + result);
+			System.out.println("#" + i + " " + maxCountY);
 		}
 	}
 }
