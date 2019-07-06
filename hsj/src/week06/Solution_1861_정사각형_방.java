cc63b9075ec58b08e9cb3385b6e26bc5d93f46cd
seongjin571 <gkstjdwls11@naver.com>
2019-07-07 02:49:55 +0900

    week06

---
+++ hsj/src/week06/Solution_1861_정사각형_방.java
@@ -0,0 +1,65 @@
+package week06;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution_1861_정사각형_방 {
+	static Queue<Integer> queue = new LinkedList<>();
+	static int[] countArray = null;
+	static int[][] map = null;
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int squareSize = 0;
+	static int count = 0;
+
+	static void doBFS() {
+		count = 0;
+		while (!queue.isEmpty()) {
+			int x = queue.poll();
+			int y = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int value = map[x][y];
+				int newX = x + dx[i];
+				int newY = y + dy[i];
+				if ((0 < newX && newX < squareSize) && (0 < newY && newY < squareSize)
+						&& map[newX][newY] == value + 1) {
+					count++;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testCount = scan.nextInt();
+		for (int i = 1; i <= testCount; i++) {
+			squareSize = scan.nextInt();
+			countArray = new int[squareSize * squareSize];
+			map = new int[squareSize][squareSize];
+			for (int j = 0; j < squareSize; j++) {
+				for (int k = 0; k < squareSize; k++) {
+					map[0][0] = scan.nextInt();
+				}
+			}
+			for (int j = 0; j < squareSize; j++) {
+				for (int k = 0; k < squareSize; k++) {
+					queue.add(j);
+					queue.add(k);
+					doBFS();
+					countArray[map[j][k]] = count;
+				}
+			}
+			int resultNum = 0;
+			int maxCount = 0;
+			for (int j = squareSize * squareSize - 1; j >= 0; j--) {
+				if (maxCount <= countArray[j]) {
+					resultNum = j;
+					maxCount = countArray[j];
+				}
+			}
+			System.out.println("#" + i + " " + resultNum + " " + maxCount);
+		}
+	}
+
+}
