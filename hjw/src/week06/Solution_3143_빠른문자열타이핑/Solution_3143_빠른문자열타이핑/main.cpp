1bfc0983a036b4040147f0200b7cf4aa84926377
unknown <kingjw007@naver.com>
2019-07-10 00:07:38 +0900

    week06

---
+++ hjw/src/week06/Solution_3143_빠른문자열타이핑/Solution_3143_빠른문자열타이핑/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	int test_case;
+	cin >> test_case;
+
+	for (int i = 0; i < test_case; i++)
+	{
+		string A;
+		string B;
+
+		cin >> A;
+		cin >> B;
+
+		int A_length;
+		int B_length;
+
+		A_length = A.length();
+		B_length = B.length();
+		
+		int number=0;
+		int j = 0;
+		while (j+B_length <= A_length) {
+			string sample;
+			sample = A.substr(j, B_length);
+			if (B.compare(sample) == 0)
+				number++;
+			j++;
+		}
+		int result = 0;
+		result = A_length - (number*B_length) + number;
+		cout << "#" << i + 1 << " " << result << endl;
+	}
+}
