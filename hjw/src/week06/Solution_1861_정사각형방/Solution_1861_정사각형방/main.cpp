aedc29ccb8307096db2f09b873d723096965405e
unknown <kingjw007@naver.com>
2019-07-12 01:13:21 +0900

    행렬찾기bfs

--- hjw/src/week06/Solution_1861_정사각형방/Solution_1861_정사각형방/main.cpp
+++ hjw/src/week06/Solution_1861_정사각형방/Solution_1861_정사각형방/main.cpp
@@ -1,75 +1,88 @@
-#include <stdio.h>
 #include<iostream>
+#include<queue>
 #include <string.h>
+#include <fstream>
 using namespace std;
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,-1,0,1 };
 int start_idx;
 int start_idy;
-void dfs(int **map,int n, int j, int k, int *number)
+int check_count;
+int max_n;
+void bfs(int **map, int x, int y, int n, int *number)
 {
-	number[map[start_idx][start_idy]]++;
-
-	for (int i = 0; i < 4; i++)
+	queue<int>que;
+	que.push(x);
+	que.push(y);
+	start_idx = x;
+	start_idy = y;
+	number[map[start_idx][start_idy]] = 1;
+	while (!que.empty())
 	{
-		int newx = j + dx[i];
-		int newy = k + dy[i];
-		if ((-1 < newx && newx < n) && (-1 < newy && newy < n) && (map[j][k] + 1 == map[newx][newy]))
+		int xx = que.front();
+		que.pop();
+		int yy = que.front();
+		que.pop();
+
+		for (int i = 0; i < 4; i++)
 		{
-			dfs(map, n, newx, newy, number);
+			int newx = xx + dx[i];
+			int newy = yy + dy[i];
+			if ((-1 < newx && newx < n) && (-1 < newy && newy < n) && (map[xx][yy] == map[newx][newy] - 1))
+			{
+				que.push(newx);
+				que.push(newy);
+				number[map[start_idx][start_idy]]++;
+			}
 		}
 	}
+	if (max_n < number[map[start_idx][start_idy]])
+		max_n = number[map[start_idx][start_idy]];
 }
 int main()
 {
 	int test_case;
-	scanf("%d", &test_case);
-
+	cin >> test_case;
+	
 	for (int i = 1; i <= test_case; i++)
 	{
 		int n;//배열의 크기
 		cin >> n;
-		int ** map;
-		map = new int*[n];
+		int loc = 0;
+		int **map = new int*[n];
 		int *number = new int[(n*n + 1)];
 
-		memset(number, 0, sizeof(int)*(n*n + 1));
-
 		for (int j = 0; j < n; j++)
+		{
 			map[j] = new int[n];
+		}
 
 		for (int j = 0; j < n; j++)
 		{
 			for (int k = 0; k < n; k++)
 				cin >> map[j][k];
 		}
-		start_idx = 0;
-		start_idy = 0;
+
+		max_n = 0;
 		for (int j = 0; j < n; j++)
 		{
 			for (int k = 0; k < n; k++)
 			{
-				start_idx = j;
-				start_idy = k;
-				dfs(map, n, j,k,number);
+				check_count = 0;
+				bfs(map, j, k, n, number);
 			}
 		}
-		int loc = 0;
-		int max = 0;
-		for (int j = 0; j <= n*n; j++)
+
+
+		for (int j = 0; j < n*n + 1; j++)
 		{
-			if (max < number[j])
-				max = number[j];
-		}
-		for (int j = 0; j <= n*n; j++)
-		{
-			if (max == number[j])
+			if (max_n == number[j])
 			{
 				loc = j;
 				break;
 			}
 		}
-		printf("#%d %d %d\n", i, loc, max);
-		
+		cout << "#" << i << " ";
+		cout << loc << " " << max_n << endl;
 	}
 }
